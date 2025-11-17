f9935c32ff6846a903a77aae0d3b7f531f568cf1
Micah Cheng <micahhlc@gmail.com>
2025-11-18 00:25:04 +0900

    update array.js

--- fundamental/array.js
+++ fundamental/array.js
@@ -13,16 +13,10 @@ function getMaxSubSum(arr) {
       sumFixedStart += arr[j];
       maxSum = Math.max(maxSum, sumFixedStart);
     }
+    console.log('first fn: ');
   }
-
   return maxSum;
 }
-
-console.log(getMaxSubSum([-1, 2, 3, -9])); // 5
-console.log(getMaxSubSum([-1, 2, 3, -9, 11])); // 11
-console.log(getMaxSubSum([-2, -1, 1, 2])); // 3
-console.log(getMaxSubSum([1, 2, 3])); // 6
-console.log(getMaxSubSum([100, -9, 2, -3, 5])); // 100
 
 function getMaxSubSum(arr) {
   let maxSum = 0;
@@ -34,13 +28,28 @@ function getMaxSubSum(arr) {
     maxSum = Math.max(maxSum, partialSum); // remember the maximum
     if (partialSum < 0) partialSum = 0; // zero if negative
   }
-
+  console.log('second fn: ');
   return maxSum;
 }
-
+/* 
 console.log(getMaxSubSum([-1, 2, 3, -9])); // 5
 console.log(getMaxSubSum([-1, 2, 3, -9, 11])); // 11
 console.log(getMaxSubSum([-2, -1, 1, 2])); // 3
-console.log(getMaxSubSum([100, -9, 2, -3, 5])); // 100
 console.log(getMaxSubSum([1, 2, 3])); // 6
-console.log(getMaxSubSum([-1, -2, -3])); // 0
+console.log(getMaxSubSum([100, -9, 2, -3, 5])); // 100 */
+/* 
+let arr = ['I', 'study', 'JavaScript', 'right', 'now'];
+console.log(arr, arr.length);
+// remove 3 first elements and replace them with another
+arr.splice(1, 1, 'really', 'am', 'studying');
+
+console.log(arr, arr.length); // now ["Let's", "dance", "right", "now"]
+ */
+let arr = [1, 2, 5];
+
+// from index -1 (one step from the end)
+// delete 0 elements,
+// then insert 3 and 4
+arr.splice(-1, 0, 3, 4);
+
+alert(arr); // 1,2,3,4,5
