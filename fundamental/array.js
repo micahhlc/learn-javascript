9828b12880a367053631b318ea6a01a41196d08b
Micah Cheng <micahhlc@gmail.com>
2025-11-23 00:46:19 +0900

    update array.js add class calculator

--- fundamental/array.js
+++ fundamental/array.js
@@ -298,9 +298,43 @@ function filterRangeInPlace_for(arr, a, b) {
 // console.log(sorted); // CSS, HTML, JavaScript
 // console.log(arr); // HTML, JavaScript, CSS (no changes)
 
-class Calculator {
-  // constructor(speed) {
-  //   this.speed = speed;
-  // }
-  calculate(str) {}
+class Calculator_simple {
+  calculate(str) {
+    return Function(`return ${str}`)();
+  }
+  addMethod(opr, func) {}
+}
+let calc = new Calculator();
+console.log(calc.calculate('3 + 7'));
+
+let powerCalc = new Calculator();
+powerCalc.addMethod('*', (a, b) => a * b);
+powerCalc.addMethod('/', (a, b) => a / b);
+powerCalc.addMethod('**', (a, b) => a ** b);
+
+result = powerCalc.calculate('2 ** 3');
+console.log(result); // 8
+
+function Calculator() {
+  this.methods = {
+    '-': (a, b) => a - b,
+    '+': (a, b) => a + b,
+  };
+
+  this.calculate = function (str) {
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1],
+      b = +split[2];
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  };
+
+  this.addMethod = function (name, func) {
+    this.methods[name] = func;
+  };
 }
