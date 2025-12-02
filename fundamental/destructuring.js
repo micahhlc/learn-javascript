11f33f8909275130834c4cf8ef59afcf18bdeae0
Micah Cheng <micahhlc@gmail.com>
2025-12-02 10:08:06 +0900

    add destructuring.js objectkeysvaluesentries.js

---
+++ fundamental/destructuring.js
@@ -0,0 +1,6 @@
+let [name1, name2, ...rest] = ['Julius', 'Caesar', 'Consul', 'of the Roman Republic'];
+
+// rest is an array of items, starting from the 3rd one
+console.log(rest[0]); // Consul
+console.log(rest[1]); // of the Roman Republic
+console.log(rest.length); // 2
--- fundamental/objectKeysValuesEntries.js
+++ fundamental/objectKeysValuesEntries.js
@@ -27,3 +27,27 @@ for (let [key, value] of Object.entries(user)) {
 
 let map = new Map(Object.entries(user));
 console.log(map);
+
+/* 
+Objects lack many methods that exist for arrays, e.g. map, filter and others.
+
+If we’d like to apply them, then we can use Object.entries followed by Object.fromEntries:
+
+Use Object.entries(obj) to get an array of key/value pairs from obj.
+Use array methods on that array, e.g. map, to transform these key/value pairs.
+Use Object.fromEntries(array) on the resulting array to turn it back into an object.
+*/
+
+let prices = {
+  banana: 1,
+  orange: 2,
+  meat: 4,
+};
+
+let doublePrices = Object.fromEntries(
+  // convert prices to array, map each key/value pair into another pair
+  // and then fromEntries gives back the object
+  Object.entries(prices).map((entry) => [entry[0], entry[1] * 2])
+);
+
+console.log(doublePrices.meat); // 8
