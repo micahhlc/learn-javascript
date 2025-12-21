be581db93cc56016b4ab0b0a777a126b172faef3
Micah Cheng <micahhlc@gmail.com>
2025-12-22 00:20:38 +0900

    add linkedlist.js

--- function/linkedlist.js
+++ function/linkedlist.js
@@ -1,4 +1,9 @@
-class LinkedList {
+/* 
+ref: https://javascript.info/recursion#linked-list
+*/
+
+
+class LinkedList {  //without tail
   constructor() {
     this.head = null;
   }
@@ -19,11 +24,45 @@ class LinkedList {
   forEach(fn) {
     let cur = this.head;
     while (cur) {
-      fn(cur.value);
+      fn(cur);
       cur = cur.next;
     }
   }
 }
+
+class LinkedListwTail {  //with tail
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  add(value) {
+    const node = { value, next: null };
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+      return;
+    }
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+  }
+
+  pop() {
+
+  }
+
+  forEach(fn) {
+    let cur = this.head;
+    while (cur) {
+      fn(cur);
+      cur = cur.next;
+    }
+  }
+}
+
 
 let list = new LinkedList();
 list.add(10);
@@ -31,9 +70,21 @@ list.add(20);
 list.add(30);
 list.add(50);
 
+let listwTail = new LinkedListwTail();
+listwTail.add(10);
+listwTail.add(20);
+listwTail.add(30);
+listwTail.add(40);
+listwTail.add(50);
+
+
 // Walk through (iterate) the list and print values
-let cur = list.head;
-while (cur) {
-  console.log(cur.value);
-  cur = cur.next;
-}
+
+console.log('-- LinkedList without Tail --')
+list.forEach(v => console.log(v.value, v.next));
+console.log('\n\n');
+console.log('-- LinkedList with Tail --')
+listwTail.forEach(v => console.log(v.value, v.next));
+console.log(`LinkedListwTail.length: ${listwTail.length}`);
+
+
