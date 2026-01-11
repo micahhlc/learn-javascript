7b17403b70f21f13fd960d588c9fc4ff85a449e3
Micah Cheng <micahhlc@gmail.com>
2026-01-11 23:14:54 +0900

    update lexicalFunction.js

---
+++ advFunction/lexicalFunction.js
@@ -0,0 +1,11 @@
+/* 
+ref: https://javascript.info/closure#lexical-environment
+
+The Lexical Environment object consists of two parts:
+
+1. Environment Record – an object that stores all local variables as its properties (and some other information like the value of this).
+2. A reference to the outer lexical environment, the one associated with the outer code.
+
+*/
+
+
